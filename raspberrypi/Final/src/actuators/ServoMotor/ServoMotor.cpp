c1e879ba73bf06b152b3b01c22a9943958c67625
Clayton Ketner <subwoof@mac.com>
2013-12-15 19:38:44 -0800

    Try close open method

--- raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
+++ raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
@@ -5,19 +5,23 @@ ServoMotor::ServoMotor(int servoNumber)
 	servoNum = servoNumber;
 	currentAngle = -1;
 
+
+}
+
+void ServoMotor::goToAngle(int angle)
+{
 	fp = fopen("/dev/servoblaster", "w");
 	if (fp == NULL)
 	{
 		printf("Error opening servoblaster file!\n");
 		exit(1);
 	}
-}
 
-void ServoMotor::goToAngle(int angle)
-{
 	fprintf(fp, "%d=%d\n", servoNum, angle);
 	delay(1000);
 	delay(abs(angle - currentAngle) * 2);
 	currentAngle = angle;
 	fflush(fp);
+
+	fclose(fp);
 }
