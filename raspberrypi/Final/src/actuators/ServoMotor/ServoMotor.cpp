c8355f7cd5794f4fda3b62aae48b9dc5dc46375c
Clayton Ketner <subwoof@mac.com>
2013-12-15 21:42:17 -0800

    Hard coded test

--- raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
+++ raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
@@ -15,8 +15,10 @@ ServoMotor::ServoMotor(int servoNumber)
 
 void ServoMotor::goToAngle(int angle)
 {
-	fprintf(fp, "%d=%d\n", servoNum, angle);
-	delay(1000);
+	if (angle < 150) { fprintf(fp, "0=75\n"); }
+	else { fprintf(fp, "0=150\n"); }
+	//fprintf(fp, "%d=%d\n", servoNum, angle);
+	delay(100);
 	delay(abs(angle - currentAngle) * 2);
 	currentAngle = angle;
 	fflush(fp);
