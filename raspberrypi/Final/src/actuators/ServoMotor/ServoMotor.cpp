4e242819ade14285b4b2ed2f58675eb01bcedcd9
Clayton Ketner <subwoof@mac.com>
2013-12-15 22:51:09 -0800

    Removed extra delay

--- raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
+++ raspberrypi/Final/src/actuators/ServoMotor/ServoMotor.cpp
@@ -18,7 +18,6 @@ void ServoMotor::goToAngle(int angle)
 	fprintf(fp, "%d=%d\n", servoNum, angle);
 	fprintf(fp, "1=100\n");
 	delay(1000);
-	delay(abs(angle - currentAngle) * 2);
 	currentAngle = angle;
 		
 	fflush(fp);
